e94c1d6c98d45a3d632d03489389a3cca0ea96a8
sangeeth <zangeed@gmail.com>
2018-02-03 17:24:48 +0530

    article controller

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -1,20 +1,40 @@
 var mongoose = require('mongoose');
-var User = mongoose.model('User');
+var Article = mongoose.model('Article');
 
 module.exports.articleRead = function(req, res) {
 
   // If no user ID exists in the JWT return a 401
   if (!req.payload._id) {
     res.status(401).json({
-      "message" : "UnauthorizedError: private profile"
+      "message" : "UnauthorizedError: private page"
     });
   } else {
     // Otherwise continue
-    User
+    Article
       .findById(req.payload._id)
       .exec(function(err, user) {
         res.status(200).json(user);
       });
   }
 
+};
+
+module.exports.articleSave = function(req, res) {
+  var article = new Article();
+
+  user.username = req.body.user.username;
+  user.email = req.body.user.email;   
+  user.setPassword(req.body.user.password);
+  user.save(function(err) {
+    var token;
+    token = user.generateJwt();
+    user.token = token;
+    res.status(200);
+    res.json({
+      "user" : user
+    });
+    if(err){
+        console.log(err);
+    }
+  });
 };
